3d1d20a0abd654107dacc762da1a6356d53d6a00
Mahmoudcr95@gmail.com <Mahmoudcr95@gmail.com>
2023-07-18 13:34:07 +0300

    --  github actions

--- test/auth_validation_test.dart
+++ test/auth_validation_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testing/main.dart';
 import 'package:testing/validation.dart';
 
 void main() {
@@ -66,4 +68,19 @@ void main() {
       expect(password, 'Password must contain at least one special character.');
     });
   });
+
+  testWidgets('Test Login Widget', (tester) async {
+    await tester.pumpWidget(const MyApp());
+    var textField = find.byType(TextField);
+    expect(textField, findsWidgets);
+    await tester.enterText(find.byKey(const Key('emailKey')), 'Mahmoud');
+    // await tester.enterText(find.byKey(const Key('emaialKey')), 'Mahmoud');
+    await tester.enterText(find.byKey(const Key('passwordKey')), '123456mA@');
+
+    var loginBtn = find.byType(ElevatedButton);
+    expect(loginBtn, findsOneWidget);
+    await tester.tap(loginBtn);
+    await tester.pump();
+  });
+
 }
